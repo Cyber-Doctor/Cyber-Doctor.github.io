dc29c47aa2028ceac74b6b66be634ffb16b533bc
Cyber-Doctor <52986424+Cyber-Doctor@users.noreply.github.com>
2019-07-17 14:37:12 +0900

    up

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,26 @@
-(function($){
-  $(function(){
-    $(".button-collapse").sideNav();
+jQuery(document).ready(function(){
+  svg4everybody();
+  $(".icon-hamburger").on('click', function(event){
+    $(".site-navigation").toggleClass('active');
+    if($(".container-search-main").hasClass('active'))
+      $(".container-search-main").toggleClass('active');
   });
-})(jQuery);
+
+  $(".icon-cross").on('click', function(event){
+    $(".site-navigation").toggleClass('active');
+  });
+
+  $(".icon-search").on('click', function(event){
+    $(".container-search-main").toggleClass('active');
+    if($(".site-navigation").hasClass('active'))
+      $(".site-navigation").toggleClass('active');
+  });
+
+  $('.anchor').click(function(e){
+    e.preventDefault();
+    setTimeout(function(){
+      $('body,html').animate({scrollTop:$('#top').offset().top},500);
+    }, 100);
+  });
+
+});
